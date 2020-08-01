8322338503fe109780492be7acefd46372ac5bb3
sungbae.chun <sungbae.chun@nu.edu.kz>
2020-08-01 13:31:55 +0600

    assignment - istream operator is being implemented

---
+++ yellow/w2/a1_template.cpp
@@ -0,0 +1,120 @@
+#include <string>
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+enum class QueryType {
+  NewBus,
+  BusesForStop,
+  StopsForBus,
+  AllBuses
+};
+
+struct Query {
+  QueryType type;
+  string bus;
+  string stop;
+  vector<string> stops;
+};
+
+istream& operator >> (istream& is, Query& q) {
+  // Реализуйте эту функцию
+  string operation_code;
+  is >> operation_code;
+  switch (operation_code) {
+    case "NEW_BUS":
+      q.bus = QueryType::NewBus;
+      is >> q.bus;
+      int stop_count;
+      is >> stop_count;
+      q.stops.resize(stop_count);
+      for (string& stop: stops){
+        is >> stop;
+      }
+      break;
+
+    case "BUSES_FOR_STOP":
+      //here
+      break;
+  }
+  return is;
+}
+
+struct BusesForStopResponse {
+  // Наполните полями эту структуру
+};
+
+ostream& operator << (ostream& os, const BusesForStopResponse& r) {
+  // Реализуйте эту функцию
+  return os;
+}
+
+struct StopsForBusResponse {
+  // Наполните полями эту структуру
+};
+
+ostream& operator << (ostream& os, const StopsForBusResponse& r) {
+  // Реализуйте эту функцию
+  return os;
+}
+
+struct AllBusesResponse {
+  // Наполните полями эту структуру
+};
+
+ostream& operator << (ostream& os, const AllBusesResponse& r) {
+  // Реализуйте эту функцию
+  return os;
+}
+
+class BusManager {
+public:
+  void AddBus(const string& bus, const vector<string>& stops) {
+    // Реализуйте этот метод
+  }
+
+  BusesForStopResponse GetBusesForStop(const string& stop) const {
+    // Реализуйте этот метод
+  }
+
+  StopsForBusResponse GetStopsForBus(const string& bus) const {
+    // Реализуйте этот метод
+  }
+
+  AllBusesResponse GetAllBuses() const {
+    // Реализуйте этот метод
+  }
+};
+
+// Не меняя тела функции main, реализуйте функции и классы выше
+
+int main() {
+  int query_count;
+  Query q;
+
+  cin >> query_count;
+
+  BusManager bm;
+  for (int i = 0; i < query_count; ++i) {
+    cin >> q;
+    switch (q.type) {
+    case QueryType::NewBus:
+      bm.AddBus(q.bus, q.stops);
+      break;
+    case QueryType::BusesForStop:
+      cout << bm.GetBusesForStop(q.stop) << endl;
+      break;
+    case QueryType::StopsForBus:
+      cout << bm.GetStopsForBus(q.bus) << endl;
+      break;
+    case QueryType::AllBuses:
+      cout << bm.GetAllBuses() << endl;
+      break;
+    }
+  }
+
+  return 0;
+}
